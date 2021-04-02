e30f2bccf7f50f805408e33029773e9c73ded3fd
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-04-02 13:17:37 -0300

    Initial Commit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+const port = 3000
 
+const indexRouter = require('./routes/index')
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.listen(3000, () => console.log(''))
+app.use('/', indexRouter)
+
+app.listen(port, () => console.log('Tabuada server on port: ' + port))
