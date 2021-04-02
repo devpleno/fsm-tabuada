d0a5a335b3770262512c2644325428861b425967
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-04-02 15:37:15 -0300

    Adicionando Assets e Finalizando

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 3000
 
 const indexRouter = require('./routes/index')
+const tabuadaRouter = require('./routes/tabuada')
 
 app.use(express.static('public'))
 
@@ -12,5 +13,6 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use('/', indexRouter)
+app.use('/tabuadas', tabuadaRouter)
 
 app.listen(port, () => console.log('Tabuada server on port: ' + port))
